2af3f97767c56a81209fb66b5e8dcccf4b9a4a00
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-05-10 12:28:22 -0400

    init

--- src/main/kotlin/com/github/jonathanlalou/mboxbackup/MboxBackupApplication.kt
+++ src/main/kotlin/com/github/jonathanlalou/mboxbackup/MboxBackupApplication.kt
@@ -1,9 +1,11 @@
 package com.github.jonathanlalou.mboxbackup
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class MboxBackupApplication
 
 fun main(args: Array<String>) {
