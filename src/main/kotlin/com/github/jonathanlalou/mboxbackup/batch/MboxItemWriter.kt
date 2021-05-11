f51cd252378e9107195d8d11f7fd978d953224c2
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-05-11 18:05:14 -0400

    cleaning

--- src/main/kotlin/com/github/jonathanlalou/mboxbackup/batch/MboxItemWriter.kt
+++ src/main/kotlin/com/github/jonathanlalou/mboxbackup/batch/MboxItemWriter.kt
@@ -3,6 +3,7 @@ package com.github.jonathanlalou.mboxbackup.batch
 import com.github.jonathanlalou.mboxbackup.domain.Mbox
 import com.github.jonathanlalou.mboxbackup.helper.MailFormatter
 import lombok.extern.log4j.Log4j
+import mu.KotlinLogging
 import org.springframework.batch.item.ItemWriter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -10,28 +11,17 @@ import org.springframework.stereotype.Component
 @Component
 @Log4j
 class MboxItemWriter : ItemWriter<Mbox> {
+    private val LOGGER = KotlinLogging.logger {}
+
     @Autowired
     lateinit var mailFormatter: MailFormatter
 
     override fun write(items: MutableList<out Mbox>) {
-        var i = 0
-        var j = 0
-
         for (mbox in items) {
-            println("WARN " + j)
-            println("***********************************")
-            println("***********************************")
-            println("***********************************")
-            println("***********************************")
-            println("***********************************")
             for (mail in mbox.mails) {
 //                FileUtils.write(File("./" + UUID.randomUUID().toString() + ".html"), mail.toString())
-                println("INFO " + mailFormatter.format(mail))
-                println("ERROR " + i)
-                i++
-//                println("INFO " + mail.toString())
+                LOGGER.info { mailFormatter.format(mail) }
             }
-            j++
         }
     }
 
