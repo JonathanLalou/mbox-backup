16c422067ea973d837c0371a49a964cfa8e8e91b
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2021-05-12 12:51:32 -0400

    assign date

--- src/main/kotlin/com/github/jonathanlalou/mboxbackup/batch/MboxItemProcessor.kt
+++ src/main/kotlin/com/github/jonathanlalou/mboxbackup/batch/MboxItemProcessor.kt
@@ -2,14 +2,19 @@ package com.github.jonathanlalou.mboxbackup.batch
 
 import com.github.jonathanlalou.mboxbackup.batch.MboxItemReader.Companion.Bcc
 import com.github.jonathanlalou.mboxbackup.batch.MboxItemReader.Companion.Cc
+import com.github.jonathanlalou.mboxbackup.batch.MboxItemReader.Companion.Date
 import com.github.jonathanlalou.mboxbackup.batch.MboxItemReader.Companion.To
 import com.github.jonathanlalou.mboxbackup.domain.Mbox
 import org.apache.commons.lang3.StringUtils
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.stereotype.Component
+import java.text.SimpleDateFormat
 
 @Component
 class MboxItemProcessor : ItemProcessor<Mbox, Mbox> {
+    val dateFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz")
+    val firstLineDateFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy")
+
     override fun process(mbox: Mbox): Mbox? {
         for (mail in mbox.mails) {
             if (mail.headers.isNotEmpty()) {
@@ -47,6 +52,19 @@ class MboxItemProcessor : ItemProcessor<Mbox, Mbox> {
                     ?.second
                     ?.split(",")
                     ?.map { StringUtils.trim(it) }
+
+                var szDate = mail.headers.filter { it.first.equals(Date) }.firstOrNull()?.second
+                if (null == szDate) {
+                    szDate = mail.raw?.substringAfter("@xxx ")
+                    mail.date = firstLineDateFormat.parse(szDate)
+                } else {
+                    try {
+                        mail.date = dateFormat.parse(szDate)
+                    } catch (e: Exception) {
+                        szDate = mail.raw?.substringAfter("@xxx ")
+                        mail.date = firstLineDateFormat.parse(szDate)
+                    }
+                }
             }
         }
         return mbox
